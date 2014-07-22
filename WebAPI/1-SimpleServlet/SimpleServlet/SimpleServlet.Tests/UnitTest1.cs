ecf503c9af32258bd243d42d4fa92e724931a0ad
Emad Hilo <emad@snickersoftware.com>
2014-07-22 13:54:15 -0500

    Adding SimpleServlet example written in WebAPI.

---
+++ WebAPI/1-SimpleServlet/SimpleServlet/SimpleServlet.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Http.Results;
+using System.Net.Http.Formatting;
+
+namespace SimpleServlet.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMsgEchoing()
+        {
+            String msg = "1234test";
+            String expectedString = "Echo:" + msg;
+
+            var controller = new Controllers.EchoController();
+
+            var result = controller.GetEcho(msg);
+            OkNegotiatedContentResult<string> conNegResult = result as OkNegotiatedContentResult<string>;
+
+            Assert.AreEqual(expectedString, conNegResult.Content);
+        }
+    }
+}
---
+++ WebAPI/1-SimpleServlet/SimpleServlet/SimpleServlet/Controllers/EchoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleServlet.Controllers
+{
+    public class EchoController : ApiController
+    {
+        public IHttpActionResult GetEcho(string msg)
+        {
+            return Ok("Echo:" + msg);
+        }
+    }
+}
